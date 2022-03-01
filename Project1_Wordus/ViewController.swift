54b1d11a7dca2b6486a6adc6af042fc38ea79134
KingstonKrunal <krunalshah661@gmail.com>
2022-03-01 16:04:42 -0500

    Initial Commit

---
+++ Project1_Wordus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1_Wordus
+//
+//  Created by Krunal Shah on 2022-03-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
