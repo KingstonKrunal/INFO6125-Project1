6a122f9e8c3fd4bce80ac02a1cc5034bb36b0664
KingstonKrunal <krunalshah661@gmail.com>
2022-03-03 02:39:29 -0500

    Letter matrix errors are gone

---
+++ Project1_Wordus/LetterCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  LetterCollectionViewCell.swift
+//  Project1_Wordus
+//
+//  Created by Krunal Shah on 2022-03-03.
+//
+
+import UIKit
+
+class LetterCollectionViewCell: UICollectionViewCell {
+    static let identifier = "LetterCollectionViewCell"
+    
+    @IBOutlet weak var letterLabel: UILabel!
+    
+}
--- Project1_Wordus/ViewController.swift
+++ Project1_Wordus/ViewController.swift
@@ -7,11 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var wordMatrix: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        wordMatrix.delegate = self
+        wordMatrix.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: LetterCollectionViewCell.identifier,
+            for: indexPath
+        ) as! LetterCollectionViewCell
+        
+        cell.letterLabel.text = "S"
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(
+            width: (wordMatrix.frame.size.width/3)-3,
+            height: (wordMatrix.frame.size.height/3)-3
+        )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+         return UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1 )
+    }
+     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("Section: \(indexPath.section) Row: \(indexPath.row)")
     }
 
 
